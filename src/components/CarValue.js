c8ad1955ec1150f9ba1d6208768159c4d07fac30
Rida Fatima <syedaridafatima67@gmail.com>
2023-06-21 16:53:33 +0500

    add value  functionality

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 function CarValue(props) {
+    const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+        return data
+            .filter((car) =>
+                car.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .reduce((acc, car) => acc + car.cost, 0);
+
+    });
     return (
-        <div>
-            CarValue
+        <div className='car-value'>
+            Total Cost: ${totalCost}
         </div>
     );
 }
